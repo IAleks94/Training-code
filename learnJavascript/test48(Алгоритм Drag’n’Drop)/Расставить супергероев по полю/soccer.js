588e1b25096f99d091659f9065a9a07cee3188f6
IAleks94 <a12344@mail.ru>
2020-01-28 11:06:26 +0300

    corrected a little

--- learnJavascript/test48(Алгоритм Drag’n’Drop)/Расставить супергероев по полю/soccer.js
+++ learnJavascript/test48(Алгоритм Drag’n’Drop)/Расставить супергероев по полю/soccer.js
@@ -9,6 +9,12 @@ let isMove = eventClick => {
   let targetSize = target.getBoundingClientRect();
   let shiftX = eventClick.clientX - targetSize.left; //от мышки до левого края элемента
   let shiftY = eventClick.clientY - targetSize.top; //от мышки до верхнего края элемента
+  //координаты экрана
+  let windowHeight = document.documentElement.clientHeight; //находим высоту "экрана"
+  let windowWidth = document.documentElement.clientWidth; //находим ширину "экрана"
+
+  let documentSize = document.documentElement.getBoundingClientRect();
+
   // вствляем элемент в конец body, меняем позицыаниирование и возвращяем на прежнее место под мышку
   document.body.append(target);
   target.style.position = "absolute";
@@ -16,25 +22,46 @@ let isMove = eventClick => {
 
   //функция для сдвигания обьекта
   let moveAt = (pageX, pageY) => {
-    target.style.left = pageX - shiftX + "px";
-    target.style.top = pageY - shiftY + "px";
+    target.style.left = pageX + "px";
+    target.style.top = pageY + "px";
   };
-  moveAt(eventClick.pageX, eventClick.pageY); //вставили цель под мышку
+  moveAt(eventClick.pageX - shiftX, eventClick.pageY - shiftY); //вставили цель под мышку
 
   //функция для движения обьекта вместе с мышкой
   let moveTarget = evtMove => {
-    // тут реализуем прокрутку экрана при нахождении обьекта на границе экрана
-    let windowHeight = document.documentElement.clientHeight; //находим высоту "экрана"
     let scrolTop = window.pageYOffset; //координаты верхней границы экрана относительно документа
     let bottomWindow = windowHeight + scrolTop; //координаты нижней границы экрана относительно документа
+    let scriolLeft = window.pageXOffset; //аналогично слева
+    let rightWindow = windowWidth + scriolLeft; //и справа
 
-    if (evtMove.pageY - shiftY < scrolTop) {
+    let Y = evtMove.pageY;
+    let X = evtMove.pageX;
+    let newY; // новые координаты запишим сюда
+    let newX;
+    //условие что бы герои не вылазили за рамки экрана
+    if (Y - shiftY < scrolTop) {
+      newY = scrolTop;
+    } else if (Y - shiftY > bottomWindow) {
+      newY = bottomWindow;
+    } else {
+      newY = Y - shiftY;
+    }
+    if (X - shiftX < scriolLeft) {
+      newX = scriolLeft;
+    } else if (X - shiftX > rightWindow) {
+      newX = rightWindow;
+    } else {
+      newX = X - shiftX;
+    }
+    // тут реализуем прокрутку экрана при нахождении обьекта на границе экрана
+
+    if (Y - shiftY < scrolTop) {
       //если верх обьекта находиться на границе - прокручеваем документ
       window.scrollBy(0, -10);
-    } else if (evtMove.pageY - shiftY + targetSize.height > bottomWindow) {
+    } else if (Y - shiftY + targetSize.height > bottomWindow && bottomWindow <= documentSize.height) {
       window.scrollBy(0, 10);
     }
-    moveAt(evtMove.pageX, evtMove.pageY);
+    moveAt(newX, newY);
   };
 
   //вешаем слушателя на цель при движение мыши
