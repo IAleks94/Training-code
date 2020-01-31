72e16b5387b668e6b7259588027068010830bb31
IAleks94 <a12344@mail.ru>
2020-01-31 12:36:53 +0300

    finis task Modal Form Dialog Box

--- learnJavascript/Часть 2 Браузер документ, события, интерфейсы/4) Формы, элементы управления/Отправка формы событие и метод submit/script.js
+++ learnJavascript/Часть 2 Браузер документ, события, интерфейсы/4) Формы, элементы управления/Отправка формы событие и метод submit/script.js
@@ -2,18 +2,90 @@
 let formContainer = document.querySelector("#prompt-form-container");
 let form = document.querySelector("#prompt-form");
 formContainer.hidden = true;
-function showPrompt(html, callback) {
-  formContainer.hidden = false;
-  let promptMessage = document.querySelector("#prompt-message");
-  promptMessage.innerHTML = html;
-  formContainer.addEventListener("click", () => {
-    event.preventDefault();
-  });
-  form.addEventListener("focusout", () => {
-    if (!event.target.closest("input")) {
-      event.target.focus();
-    }
-  });
+// Показать полупрозрачный DIV, чтобы затенить страницу
+// (форма располагается не внутри него, а рядом, потому что она не должна быть полупрозрачной)
+function showCover() {
+  let coverDiv = document.createElement("div");
+  coverDiv.id = "cover-div";
+  // убираем возможность прокрутки страницы во время показа модального окна с формой
+  document.body.style.overflowY = "hidden";
+
+  document.body.append(coverDiv);
 }
 
-showPrompt("Введите что-нибудь<br>...умное :)");
+function hideCover() {
+  document.getElementById("cover-div").remove();
+  document.body.style.overflowY = "";
+}
+
+function showPrompt(html, callback) {
+  showCover();
+  formContainer.hidden = false;
+  form.text.focus();
+  let promptMessage = document.querySelector("#prompt-message");
+  promptMessage.innerHTML = html;
+
+  let firstElement = form.elements[0];
+  let lastElement = form.elements[form.elements.length - 1];
+
+  // функция отслеживающая табы и эскейпы
+  function onWhom(event) {
+    let target = event.target;
+   switch (target) {
+      case lastElement:
+        if (event.key === "Tab" && !event.shiftKey) {
+          event.preventDefault();
+          firstElement.focus();
+          
+        }
+        break;
+      case firstElement:
+        if (event.key === "Tab" && event.shiftKey) {
+          event.preventDefault();
+          lastElement.focus();
+        }
+        //без этого при выделении поля ввода на эскейп ничего не срабатывает
+        if (event.key === "Escape") {
+          complete(null);
+        }
+        break;
+
+      default:
+        if (event.key === "Escape") {
+          complete(null);
+        }
+        break;
+    }
+  }
+  function complete(value) {
+    hideCover();
+    formContainer.hidden = true;
+    document.removeEventListener("keydown", onWhom);
+    callback(value);
+  }
+
+  function submit(event) {
+    event.preventDefault();
+    let value = form.text.value;
+    if (value == "") return; // игнорируем отправку пустой формы
+
+    complete(value);
+  }
+
+  function isCancel(event) {
+    let target = event.target;
+    if (target === form.cancel) {
+      complete(null);
+    }
+  }
+
+  form.addEventListener("submit", submit);
+  form.addEventListener("click", isCancel);
+  document.addEventListener("keydown", onWhom);
+}
+
+document.getElementById("show-button").addEventListener("click", () => {
+  showPrompt("Введите что-нибудь<br>...умное :)", function(value) {
+    alert("Вы ввели: " + value);
+  });
+});
